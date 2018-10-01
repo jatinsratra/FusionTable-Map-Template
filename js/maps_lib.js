e94df2fb125060f5aded0e0dc68d4ac363a6d1a2
jatinsratra <43587254+jatinsratra@users.noreply.github.com>
2018-10-02 00:17:34 +0530

    Update maps_lib.js

--- js/maps_lib.js
+++ js/maps_lib.js
@@ -178,7 +178,7 @@
         self.whereClause = self.locationColumn + " not equal to ''";
         
         //-----custom filters-----
-        var type_column = "'typeNum'";
+        var type_column = "'Filter'";
 var searchType = type_column + " IN (-1,";
 if ( $("#cbType1").is(':checked')) searchType += "1,";
 if ( $("#cbType2").is(':checked')) searchType += "2,";
